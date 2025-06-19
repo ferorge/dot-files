f2c9db2161885de72aad85f8ea30e6e2e6762deb
ferorge <ferorge@landing>
2025-06-19 12:38:58 +0300

    Corrección de respaldo.

--- install-dot-files.sh
+++ install-dot-files.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
-DIR="~/dot-files/files/"
+DIR=~/dot-files/files/
 timestamp=$(date +%F_%H.%M.%S)
 
 for dot in $(ls -A1 $DIR);
 do
-    mv ~/$dot ~/dot-files/backup/$dot_$timestamp
+    mv ~/$dot ~/dot-files/backup/$dot.$timestamp
     ln $DIR$dot ~/;
 done
