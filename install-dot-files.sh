d599d9bb6b3fe2d58570a4c97009a9bae05aec25
ferorge <ferorge@landing>
2025-06-19 12:26:23 +0300

    Primera confirmación.

---
+++ install-dot-files.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+DIR="~/dot-files/files/"
+timestamp=$(date +%F_%H.%M.%S)
+
+for dot in $(ls -A1 $DIR);
+do
+    mv ~/$dot ~/dot-files/backup/$dot_$timestamp
+    ln $DIR$dot ~/;
+done
