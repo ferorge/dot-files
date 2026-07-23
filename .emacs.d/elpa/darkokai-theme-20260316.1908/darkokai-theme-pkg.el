9f8581b836bedf901ef360eb35ac044003d7db56
ferorge <ferorge@sobnix.ar>
2026-07-23 13:33:24 -0300

    epla | Primera confirmación

---
+++ .emacs.d/elpa/darkokai-theme-20260316.1908/darkokai-theme-pkg.el
@@ -0,0 +1,7 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "darkokai-theme" "20260316.1908"
+  "A darker variant on Monokai."
+  ()
+  :url "https://github.com/sjrmanning/darkokai"
+  :commit "bd1e2bc33a81b331cc7747cd2f405f793348061d"
+  :revdesc "bd1e2bc33a81")
---
+++ .emacs.d/elpa/monokai-alt-theme-20260303.647/monokai-alt-theme-pkg.el
@@ -0,0 +1,7 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "monokai-alt-theme" "20260303.647"
+  "Theme with a dark background. Based on sublime monokai theme."
+  '((emacs "24"))
+  :url "https://github.com/dawidof/emacs-monokai-theme"
+  :commit "864ab2c8c32e50f0ec6f1563107152814e4a0e38"
+  :revdesc "864ab2c8c32e")
